20670b22ee0969fa5ffd11f0865affd0e6640821
ahmad-aljabali <36141448+ahmad-aljabali@users.noreply.github.com>
2020-04-09 23:07:00 +0300

    Update ubuntu_fresh_install_setup.sh

--- ubuntu_fresh_install_setup.sh
+++ ubuntu_fresh_install_setup.sh
@@ -1,22 +1,27 @@
 sudo apt update
 sudo apt upgrade
 
+# CUDA
+sudo apt install nvidia-cuda-toolkit nvidia-modprobe
+
 sudo apt install gnome-tweaks 
 sudo apt install synaptic
 sudo apt install htop
 sudo apt install tilix
 
 # install chrome
-#wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-#sudo dpkg -i google-chrome-stable_current_amd64.deb
-#rm -f ./google-chrome-stable_current_amd64.deb
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg -i google-chrome-stable_current_amd64.deb
+rm -f ./google-chrome-stable_current_amd64.deb
 
 sudo apt install steam
 sudo apt install transmission
 sudo apt install vlc
 sudo apt install spotify-client
 
-sudo apt install blender
+sudo apt install snapd
+sudo snap install blender --classic
+#sudo apt install blender
 sudo apt install rawtherapee
 sudo apt install gimp
 
